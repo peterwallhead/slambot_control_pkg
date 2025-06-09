36596a33fb04610148b07089d86c5544bcb17303
Peter Wallhead <peter.wallhead@gmail.com>
2025-06-09 14:38:06 +1000

    Send velocities to the Arduino Mega

--- slambot_control_pkg/motor_driver_bridge.py
+++ slambot_control_pkg/motor_driver_bridge.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
+import argparse
+
 import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
+import serial
 
 class MotorDriverBridge(Node):
-    def __init__(self):
+    def __init__(self, motor_driver_port):
         super().__init__("motor_driver_bridge")
+
+        self.ser = serial.Serial(motor_driver_port, 115200, timeout=10)
 
         self.geometry_subscriber = self.create_subscription(Twist, "cmd_vel", self.callback_command_motors, 10)
 
@@ -17,9 +22,17 @@ class MotorDriverBridge(Node):
 
         self.get_logger().info(f"linear.x: {linear_x}, angular.z: {angular_z}")
 
+        command = f"V{linear_x:.2f},{angular_z:.2f}\n"
+        self.ser.write(command.encode('utf-8'))
+
 def main(args=None):
-    rclpy.init(args=args)
-    node = MotorDriverBridge()
+    parser = argparse.ArgumentParser(description='MotorDriverBridge')
+    parser.add_argument('--motor_driver_port', type=str, default='/dev/ttyUSB0', help='Set motor driver (Arduino Mega) port')
+
+    user_args, ros_args = parser.parse_known_args()
+
+    rclpy.init(args=ros_args)
+    node = MotorDriverBridge(motor_driver_port=user_args.motor_driver_port)
     rclpy.spin(node)
     rclpy.shutdown()
 
